02cfc8c3a69e5c41806c659695fc9d3185830a52
abdulloh <abdullohjorayevv7@gmail.com>
2024-06-01 14:29:59 +0500

    1. Two Sum

---
+++ two-sum/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().twoSum(new int[] {2, 7, 11, 15}, 9));
+    }
+    
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
