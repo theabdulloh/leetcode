c6af51fbf7e113a7a04ad5ff7c96c53fe65a7dc4
abdulloh <abdullohjorayevv7@gmail.com>
2024-06-04 15:04:57 +0500

    2235. Add Two Integers

---
+++ add-two-integers/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().sum(12, 5));
+    }
+    
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
