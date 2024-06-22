c6aff2e1b3d3d89be9972f9b4ef136f3560af9ce
abdulloh <abdullohjorayevv7@gmail.com>
2024-06-23 00:02:15 +0500

    709. To Lower Case

---
+++ to-lower-case/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().toLowerCase("Hello"));
+    }
+    
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
